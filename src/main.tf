6753007950cb4e867a774f8cf45eee402d31c7b8
Im4i <rodrigoimai@hotmail.com>
2024-09-18 21:14:14 -0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -9,7 +9,6 @@ module "vpc" {
   public_subnets  = var.aws_vpc_public_subnets
 
   enable_nat_gateway = true
-  enable_vpn_gateway = true
 
   tags = merge(var.aws_project_tags, { "kubernetes.io/cluster/${var.aws_eks_name}" = "shared" })
 
