91e6b64e70ed12e9a18f1269e425a30c2af00a73
im4i <rodrigoimai@hotmail.com>
2024-09-19 18:59:19 -0300

    Adiciona grupo de segurança

--- src/main.tf
+++ src/main.tf
@@ -24,5 +24,6 @@ resource "aws_eks_cluster" "shogun" {
 
   vpc_config {
     subnet_ids = ["subnet-08b12ee2b94a5858c", "subnet-0a101f1266c9eac44", "subnet-0b4d0b0ab35c17f9b", "subnet-007d71cbdfb479ac0", "subnet-0c3fe33974de67d07", "subnet-0f31e291536aba412"]
+    security_group_ids = ["sg-0c5ae984e06c36361"]
   }
 }
