775b04e3f47ecd6f16b84bc3eb3fba1ac34893cb
im4i <rodrigoimai@hotmail.com>
2024-09-20 21:23:25 -0300

    Refatora EKS

--- src/main.tf
+++ src/main.tf
@@ -12,18 +12,43 @@ terraform {
   }
 }
 
-variable "node_role_arn" {
-  type      = string
-  sensitive = true
-}
-
-resource "aws_eks_cluster" "shogun" {
-  name     = "shogun_cluster"
+# Cria o cluster do EKS
+resource "aws_eks_cluster" "shogun_cluster" {
+  name     = var.aws_eks_cluster_name
   role_arn = var.node_role_arn
 
-
   vpc_config {
-    subnet_ids = ["subnet-08b12ee2b94a5858c", "subnet-0a101f1266c9eac44", "subnet-0b4d0b0ab35c17f9b", "subnet-0c3fe33974de67d07", "subnet-0f31e291536aba412"]
-    security_group_ids = ["sg-0c5ae984e06c36361"]
+    subnet_ids = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id, aws_subnet.subnet_4.id, aws_subnet.subnet_5.id]
+    security_group_ids = [var.security_group_id]
+  }
+}
+
+# Cria o node group
+resource "aws_eks_node_group" "example" {
+  cluster_name    = var.aws_eks_cluster_name
+  node_group_name = var.node_group_name
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
+
+  scaling_config {
+    desired_size = 1
+    max_size     = 2
+    min_size     = 1
+  }
+
+  update_config {
+    max_unavailable = 1
+  }
+}
+
+# Configura Fargate
+resource "aws_eks_fargate_profile" "example" {
+  cluster_name           =  var.aws_eks_cluster_name
+  fargate_profile_name   = "example"
+  pod_execution_role_arn = var.node_role_arn
+  subnet_ids             = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id, aws_subnet.subnet_3.id, aws_subnet.subnet_4.id, aws_subnet.subnet_5.id]
+
+  selector {
+    namespace = "shogun"
   }
 }
