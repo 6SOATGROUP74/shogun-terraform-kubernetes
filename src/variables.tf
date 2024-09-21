2e34c4c58dc845e9d92574729f61ae36a7b59e8f
im4i <rodrigoimai@hotmail.com>
2024-09-21 00:04:05 -0300

    Remove variaveis sem uso

--- src/variables.tf
+++ src/variables.tf
@@ -8,11 +8,6 @@ variable "aws_eks_cluster_name" {
   default = "shogun_cluster"
 }
 
-variable "security_group_id" {
-  type    = string
-  default = "sg-0f625f07685998413"
-}
-
 variable "node_group_name" {
   type    = string
   default = "shogun_node_group"
