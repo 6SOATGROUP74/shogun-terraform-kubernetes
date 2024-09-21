cae6e1bd4c086b3c9781ad2d0e2b069910d07707
im4i <rodrigoimai@hotmail.com>
2024-09-20 21:52:17 -0300

    Altera região

--- src/vpc.tf
+++ src/vpc.tf
@@ -6,30 +6,35 @@ resource "aws_vpc" "shogun_vpc" {
 
 # Subnet 1
 resource "aws_subnet" "subnet_1" {
-  vpc_id     = aws_vpc.shogun_vpc.id
-  cidr_block = "172.31.0.0/20"
+  vpc_id            = aws_vpc.shogun_vpc.id
+  cidr_block        = "172.31.0.0/20"
+  availability_zone = "us-east-1a"
 }
 
 # Subnet 2
 resource "aws_subnet" "subnet_2" {
-  vpc_id     = aws_vpc.shogun_vpc.id
-  cidr_block = "172.31.80.0/20"
+  vpc_id            = aws_vpc.shogun_vpc.id
+  cidr_block        = "172.31.80.0/20"
+  availability_zone = "us-east-1b"
 }
 
 # Subnet 3
 resource "aws_subnet" "subnet_3" {
   vpc_id     = aws_vpc.shogun_vpc.id
   cidr_block = "172.31.32.0/20"
+  availability_zone = "us-east-1c"
 }
 
 # Subnet 4
 resource "aws_subnet" "subnet_4" {
   vpc_id     = aws_vpc.shogun_vpc.id
   cidr_block = "172.31.64.0/20"
+  availability_zone = "us-east-1e"
 }
 
 # Subnet 5
 resource "aws_subnet" "subnet_5" {
   vpc_id     = aws_vpc.shogun_vpc.id
   cidr_block = "172.31.16.0/20"
+  availability_zone = "us-east-1f"
 }
