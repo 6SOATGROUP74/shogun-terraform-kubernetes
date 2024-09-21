33834c68a0a13853e749e7e316c0e7ac74206788
im4i <rodrigoimai@hotmail.com>
2024-09-20 22:16:47 -0300

    Cria grupo de segurança

--- src/vpc.tf
+++ src/vpc.tf
@@ -17,3 +17,34 @@ resource "aws_subnet" "subnet_2" {
   cidr_block        = "172.31.80.0/20"
   availability_zone = "us-east-1b"
 }
+
+# Cria grupo de segurança VPC
+resource "aws_security_group" "shogun_grupo" {
+  name        = "shogun_grupo_de_segurança"
+  description = "Grupo de segurança dos serviços Shogun"
+  vpc_id      = aws_vpc.shogun_vpc
+
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ipv4_shogun" {
+  security_group_id = aws_security_group.shogun_grupo.id
+  cidr_ipv4         = aws_vpc.shogun_vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ipv4_shogun_base" {
+  security_group_id = aws_security_group.shogun_grupo.id
+  cidr_ipv4         = aws_vpc.shogun_vpc.cidr_block
+  from_port         = 3306
+  ip_protocol       = "tcp"
+  to_port           = 3306
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "ipv4_shogun" {
+  security_group_id = aws_security_group.shogun_grupo.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
