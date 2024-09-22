579cf1dcce36bebfdead585141ebc789037d0a69
im4i <rodrigoimai@hotmail.com>
2024-09-21 23:56:41 -0300

    Adiciona novas regras

--- src/main.tf
+++ src/main.tf
@@ -23,6 +23,26 @@ resource "aws_eks_cluster" "shogun_cluster" {
   }
 }
 
+resource "aws_iam_role" "fargate_shogun_role" {
+  name = "fargate_shogun_role"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "eks-fargate-pods.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "shogun-AmazonEKSFargatePodExecutionRolePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+  role       = aws_iam_role.fargate_shogun_role.name
+}
+
 # Configura Fargate
 resource "aws_eks_fargate_profile" "eks_fargate" {
 
@@ -32,12 +52,43 @@ resource "aws_eks_fargate_profile" "eks_fargate" {
 
   cluster_name           = var.aws_eks_cluster_name
   fargate_profile_name   = var.fargate_name
-  pod_execution_role_arn = "arn:aws:iam::342326109351:role/aws-service-role/eks-fargate.amazonaws.com/AWSServiceRoleForAmazonEKSForFargate"
+  pod_execution_role_arn = aws_iam_role.fargate_shogun_role.arn
   subnet_ids             = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
   selector {
     namespace = "shogun"
   }
+}
+
+# Roles do node group
+resource "aws_iam_role" "eks_shogun_node_group" {
+  name = "eks_shogun_node_group"
+
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "shogun-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_shogun_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "shogun-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_shogun_node_group.name
+}
+
+resource "aws_iam_role_policy_attachment" "shogun-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_shogun_node_group.name
 }
 
 # Cria o node group
@@ -49,7 +100,7 @@ resource "aws_eks_node_group" "aws_eks_node_group_shogun" {
 
   cluster_name    = var.aws_eks_cluster_name
   node_group_name = var.node_group_name
-  node_role_arn   = "arn:aws:iam::342326109351:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
+  node_role_arn   = aws_iam_role.eks_shogun_node_group.arn
   subnet_ids      = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
   scaling_config {
