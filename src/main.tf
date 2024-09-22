ccf848f32e70015cb5fd7f85139e0c6303c06cc8
im4i <rodrigoimai@hotmail.com>
2024-09-21 23:33:18 -0300

    Corrigie politicas

--- src/main.tf
+++ src/main.tf
@@ -15,7 +15,7 @@ terraform {
 # Cria o cluster do EKS
 resource "aws_eks_cluster" "shogun_cluster" {
   name     = var.aws_eks_cluster_name
-  role_arn = [var.node_role_arn, "arn:aws:iam::342326109351:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup", "arn:aws:iam::342326109351:role/aws-service-role/eks-fargate.amazonaws.com/AWSServiceRoleForAmazonEKSForFargate"]
+  role_arn = var.node_role_arn
 
   vpc_config {
     subnet_ids         = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
@@ -32,7 +32,7 @@ resource "aws_eks_fargate_profile" "eks_fargate" {
 
   cluster_name           = var.aws_eks_cluster_name
   fargate_profile_name   = var.fargate_name
-  pod_execution_role_arn = [var.node_role_arn, "arn:aws:iam::342326109351:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup", "arn:aws:iam::342326109351:role/aws-service-role/eks-fargate.amazonaws.com/AWSServiceRoleForAmazonEKSForFargate"]
+  pod_execution_role_arn = "arn:aws:iam::342326109351:role/aws-service-role/eks-fargate.amazonaws.com/AWSServiceRoleForAmazonEKSForFargate"
   subnet_ids             = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
   selector {
@@ -49,7 +49,7 @@ resource "aws_eks_node_group" "aws_eks_node_group_shogun" {
 
   cluster_name    = var.aws_eks_cluster_name
   node_group_name = var.node_group_name
-  node_role_arn   = [var.node_role_arn, "arn:aws:iam::342326109351:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup", "arn:aws:iam::342326109351:role/aws-service-role/eks-fargate.amazonaws.com/AWSServiceRoleForAmazonEKSForFargate"]
+  node_role_arn   = "arn:aws:iam::342326109351:role/aws-service-role/eks-nodegroup.amazonaws.com/AWSServiceRoleForAmazonEKSNodegroup"
   subnet_ids      = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
 
   scaling_config {
