f41b9c56c0ce5ee358a4b2d7f292170157c59044
im4i <rodrigoimai@hotmail.com>
2024-09-22 18:24:42 -0300

    Remove addons

--- src/main.tf
+++ src/main.tf
@@ -40,41 +40,41 @@ resource "aws_eks_fargate_profile" "eks_fargate" {
   }
 }
 
-# Adiciona recursos para gerenciar o cluster
-resource "aws_eks_addon" "addons" {
-  for_each                    = {for addon in var.addons : addon.name => addon}
-  cluster_name                = aws_eks_cluster.shogun_cluster.name
-  addon_name                  = each.value.name
-  addon_version               = each.value.version
-  resolve_conflicts_on_create = "OVERWRITE"
-  service_account_role_arn    = var.node_role_arn
-
-  depends_on = [
-    aws_eks_cluster.shogun_cluster,
-    aws_eks_fargate_profile.eks_fargate
-  ]
-}
-
-variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
-  default = [
-    {
-      name    = "kube-proxy"
-      version = "v1.30.0-eksbuild.3"
-    },
-    {
-      name    = "vpc-cni"
-      version = "v1.18.3-eksbuild.3"
-    },
-    {
-      name    = "coredns"
-      version = "v1.11.1-eksbuild.9"
-    }
-  ]
-}
+# # Adiciona recursos para gerenciar o cluster
+# resource "aws_eks_addon" "addons" {
+#   for_each                    = {for addon in var.addons : addon.name => addon}
+#   cluster_name                = aws_eks_cluster.shogun_cluster.name
+#   addon_name                  = each.value.name
+#   addon_version               = each.value.version
+#   resolve_conflicts_on_create = "OVERWRITE"
+#   service_account_role_arn    = var.node_role_arn
+#
+#   depends_on = [
+#     aws_eks_cluster.shogun_cluster,
+#     aws_eks_fargate_profile.eks_fargate
+#   ]
+# }
+#
+# variable "addons" {
+#   type = list(object({
+#     name    = string
+#     version = string
+#   }))
+#   default = [
+#     {
+#       name    = "kube-proxy"
+#       version = "v1.30.0-eksbuild.3"
+#     },
+#     {
+#       name    = "vpc-cni"
+#       version = "v1.18.3-eksbuild.3"
+#     },
+#     {
+#       name    = "coredns"
+#       version = "v1.11.1-eksbuild.9"
+#     }
+#   ]
+# }
 
 # Cria o node group
 resource "aws_eks_node_group" "aws_eks_node_group_shogun" {
