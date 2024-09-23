3fe3c8b7042710a54f9f692a48da059489be51cc
im4i <rodrigoimai@hotmail.com>
2024-09-23 20:51:41 -0300

    Remove vpc e fargate

--- src/main.tf
+++ src/main.tf
@@ -23,59 +23,6 @@ resource "aws_eks_cluster" "shogun_cluster" {
   }
 }
 
-# # Configura Fargate
-# resource "aws_eks_fargate_profile" "eks_fargate" {
-#
-#   depends_on = [
-#     aws_eks_cluster.shogun_cluster
-#   ]
-#
-#   cluster_name           = var.aws_eks_cluster_name
-#   fargate_profile_name   = var.fargate_name
-#   pod_execution_role_arn = var.node_role_arn
-#   subnet_ids = ["subnet-0b4d0b0ab35c17f9b", "subnet-0c3fe33974de67d07"]
-#
-#   selector {
-#     namespace = "app-pod"
-#   }
-# }
-
-# # Adiciona recursos para gerenciar o cluster
-# resource "aws_eks_addon" "addons" {
-#   for_each                    = {for addon in var.addons : addon.name => addon}
-#   cluster_name                = aws_eks_cluster.shogun_cluster.name
-#   addon_name                  = each.value.name
-#   addon_version               = each.value.version
-#   resolve_conflicts_on_create = "OVERWRITE"
-#   service_account_role_arn    = var.node_role_arn
-#
-#   depends_on = [
-#     aws_eks_cluster.shogun_cluster,
-#     aws_eks_fargate_profile.eks_fargate
-#   ]
-# }
-#
-# variable "addons" {
-#   type = list(object({
-#     name    = string
-#     version = string
-#   }))
-#   default = [
-#     {
-#       name    = "kube-proxy"
-#       version = "v1.30.0-eksbuild.3"
-#     },
-#     {
-#       name    = "vpc-cni"
-#       version = "v1.18.3-eksbuild.3"
-#     },
-#     {
-#       name    = "coredns"
-#       version = "v1.11.1-eksbuild.9"
-#     }
-#   ]
-# }
-
 # Cria o node group
 resource "aws_eks_node_group" "aws_eks_node_group_shogun" {
 
