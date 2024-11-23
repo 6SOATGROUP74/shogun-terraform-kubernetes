a2a12b9e481b44c400698d140e65494e18b40a19
igusvs <igoralves159@gmail.com>
2024-11-23 18:47:59 -0300

    ajuste role lab

--- src/local.tf
+++ src/local.tf
@@ -1,6 +1,6 @@
 locals {
   cluster_name = "shogun-cluster-eks"
   region       = "us-east-1"
-  lab_role     = "arn:aws:iam::410052166411:role/LabRole"
+  lab_role     = "arn:aws:iam::126003640773:role/LabRole"
   subnets      = ["subnet-0d18e76cc85fb2e41", "subnet-03a92073745b1f361"]
 }
