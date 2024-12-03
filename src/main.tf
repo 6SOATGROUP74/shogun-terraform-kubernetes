9491b915683e3e645085116b52ad61015ff8e6da
igusvs <igoralves159@gmail.com>
2024-12-02 22:16:55 -0300

    rebuild

--- src/main.tf
+++ src/main.tf
@@ -39,7 +39,7 @@ resource "aws_eks_node_group" "aws_eks_node_group_shogun" {
   scaling_config {
     desired_size = 3
     max_size     = 4
-    min_size     = 13
+    min_size     = 3
   }
 
   update_config {
