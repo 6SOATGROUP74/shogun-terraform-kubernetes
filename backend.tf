25764b734b3372026e6d36c952e955e0740c3cbd
igusvs <igoralves159@gmail.com>
2024-12-02 21:03:41 -0300

    ajuste role e backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "backend-terraform-shogun"
+    bucket = "backend-terraform-shogun-fiap"
     key = "k8s/terraform.tfstate"
     region = "us-east-1"
   }
